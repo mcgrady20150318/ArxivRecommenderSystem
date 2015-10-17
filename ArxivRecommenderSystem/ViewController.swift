34c3e174c4d4c6ed96985f2e7de0327c038d343b
mcgrady164 <mcgrady150318@163.com>
2015-10-16 17:44:58 -0700

    create a project

---
+++ ArxivRecommenderSystem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArxivRecommenderSystem
+//
+//  Created by zhangjun on 15/10/16.
+//  Copyright © 2015年 zhangjun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
